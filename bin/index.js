b22009432879ff613c40039c6a4b348bc14fba51
Ben Hastings <bhastings@brightsign.biz>
2023-05-05 13:23:53 -0700

    Add jq notes
    * Added jq installation and usage examples

--- bin/index.js
+++ bin/index.js
@@ -14,6 +14,7 @@ const options = yargs
   .option('p', { alias: 'targetPassword', describe: 'Password of Target Player', type: 'string', demandOption: false })
   .option('m', { alias: 'reqMethod', describe: 'Request method type', type: 'string', demandOption: true })
   .option('r', { alias: 'reqRoutes', describe: 'Request url route', type: 'string', demandOption: true })
+  .option('a', { alias: 'raw', describe: 'Raw HTTP REST Response', type: 'boolean', demandOption: false })
   // .option('s', { alias: 'dest', describe: 'Target Destination. e.g., sd/myfolder/', type: 'string', demandOption: false })
   // .option('f', { alias: 'file', describe: 'File name or source file path to upload to target player', type: 'string', demandOption: true })
   .argv;
@@ -23,6 +24,7 @@ let ipAddress = options.i;
 let targetPassword = options.p;
 let requestMethod = options.m;
 let requestRoute = options.r;
+let rawResponse = options.a;
 
 // PUT e.g., /files/sd + file blob. 
 // let destDirectory = options.s;
@@ -47,9 +49,10 @@ async function requestAxios (requestOptions) {
 
     requestOptions.url = `http://${ipAddress}/api/v1` + requestOptions.url;
     response = await axiosDigestAuthInst.request(requestOptions);
-
-    if (response.status === 200) {
-      console.log(`Response status ${response.status}`);
+    if (rawResponse) {
+      console.log(JSON.stringify(response));
+    } else {
+      console.log(JSON.stringify(response.data));
     }
     return response.data?.data.result || response.data.result;
 };
@@ -57,9 +60,9 @@ async function requestAxios (requestOptions) {
 async function main() {
   try {
     let response = await requestAxios({method: `${requestMethod}`, url: `${requestRoute}`});
-    console.log(`Request response: ${JSON.stringify(response)}`);
+    // console.log(JSON.stringify(response));
   } catch (e) {
-    console.log(`Request error: ${e}`);
+    console.log(JSON.stringify(e));
   }
 }
 
