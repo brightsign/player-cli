b9d0fb031a16681b08069f003e6e710299f14e30
Ben Hastings <bhastings@brightsign.biz>
2023-05-05 12:37:33 -0700

    Initial commit
    * Added digest auth support
    * Implementeded generic Local DWS API supportability through cli args

---
+++ bin/index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const AxiosDigestAuth = require('@mhoc/axios-digest-auth').default;
+
+// Set up CLI
+const options = yargs
+  .usage('Usage: bsc -i <targetIp> -p <targetPassword> -m <reqMethod> -r <reqRoute>')
+  .usage('')
+  .usage('       Examples:')
+  .usage('       bsc -i=192.168.128.148 -p=ABC01A000001 -m=GET -r="/info"')
+  .usage('       bsc -i=192.168.128.148 -p=ABC01A000001 -m=GET -r="/files/sd"')
+  .option('i', { alias: 'targetIp', describe: 'IP Address of Target Player', type: 'string', demandOption: true })
+  .option('p', { alias: 'targetPassword', describe: 'Password of Target Player', type: 'string', demandOption: false })
+  .option('m', { alias: 'reqMethod', describe: 'Request method type', type: 'string', demandOption: true })
+  .option('r', { alias: 'reqRoutes', describe: 'Request url route', type: 'string', demandOption: true })
+  // .option('s', { alias: 'dest', describe: 'Target Destination. e.g., sd/myfolder/', type: 'string', demandOption: false })
+  // .option('f', { alias: 'file', describe: 'File name or source file path to upload to target player', type: 'string', demandOption: true })
+  .argv;
+
+// CLI Arguments
+let ipAddress = options.i;
+let targetPassword = options.p;
+let requestMethod = options.m;
+let requestRoute = options.r;
+
+// PUT e.g., /files/sd + file blob. 
+// let destDirectory = options.s;
+// let filePath = options.f;
+
+let axiosDigestAuthInst;
+
+async function prepareAxios () {
+    if (axiosDigestAuthInst == null || axiosDigestAuthInst === undefined) {
+        const options = {
+            username: 'admin',
+            password: targetPassword,
+        };
+        axiosDigestAuthInst = new AxiosDigestAuth(options);
+    }
+};
+
+async function requestAxios (requestOptions) {
+    let response;
+
+    prepareAxios();
+
+    requestOptions.url = `http://${ipAddress}/api/v1` + requestOptions.url;
+    response = await axiosDigestAuthInst.request(requestOptions);
+
+    if (response.status === 200) {
+      console.log(`Response status ${response.status}`);
+    }
+    return response.data?.data.result || response.data.result;
+};
+
+async function main() {
+  try {
+    let response = await requestAxios({method: `${requestMethod}`, url: `${requestRoute}`});
+    console.log(`Request response: ${JSON.stringify(response)}`);
+  } catch (e) {
+    console.log(`Request error: ${e}`);
+  }
+}
+
+if (require.main === module) {
+	main();
+}
